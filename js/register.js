3b8713257ee1f082143b568c502b582a459ce49d
Dora-Lencse <dora.lencse@gmail.com>
2018-07-12 21:45:34 +0200

    First commit

---
+++ js/register.js
@@ -0,0 +1,49 @@
+
+  /**
+   *     Service Worker registration
+   */
+ 'use strict';
+// Register service worker (src: https://stackoverflow.com/questions/36828088/serviceworkerregistration-active-not-being-set-first-time-chrome)
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(onRegistration);
+}
+
+function onRegistration(registration) {
+  if (registration.waiting) {
+    console.log('waiting', registration.waiting);
+    registration.waiting.addEventListener('statechange', onStateChange('waiting'));
+  }
+
+  if (registration.installing) {
+    console.log('installing', registration.installing);
+    registration.installing.addEventListener('statechange', onStateChange('installing'));
+  }
+
+  if (registration.active) {
+    console.log('active', registration.active);
+    registration.active.addEventListener('statechange', onStateChange('active'));
+  }
+}
+
+function onStateChange(from) {
+  return function(e) {
+    console.log('statechange', from, 'to', e.target.state);
+  }
+}
+
+/* // Check if service worker is supported in the browser
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+            console.log(`Registration successful, scope is ${registration.scope}`);
+        }).catch(error => {
+            console.log(`Service worker registration failed, error: ${error}`);
+        });
+}
+// Register service worker
+navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+        registration.unregister();
+    }
+}) */
---
+++ sw.js
@@ -0,0 +1,81 @@
+// Empty the old cache for testing
+caches.keys().then(names => {
+    for (let name of names)
+        caches.delete(name);
+});
+
+// Create a version and a cache name
+const staticCacheName = 'restaurant-static';
+
+// Cache all the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll([
+          '/',
+          'index.html',
+          'restaurant.html',
+		  'sw.js',
+          'css/styles.css',
+          'js/dbhelper.js',
+          'js/main.js',
+          'js/register_sw.js',
+          'data/restaurants.json',
+		  'img/1.jpg',
+		  'img/2.jpg',
+		  'img/3.jpg',
+		  'img/4.jpg',
+		  'img/5.jpg',
+		  'img/6.jpg',
+		  'img/7.jpg',
+		  'img/8.jpg',
+		  'img/9.jpg',
+		  'img/10.jpg',
+		  'offline/img/failed-internet.jpg',
+		  'offline/offline.html',
+        ]).catch(error => {
+        console.log('Caches opening failed: ' + error);
+      });
+    })
+  );
+});
+
+// Remove outdated cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    // caches.delete('-restaurant-static-001')
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('restaurant-') && cacheName !== staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Cache falling back to the network: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#cachefallback
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+     return fetch(event.request).then(response => {
+        if (response.status === 404) {
+          console.log(response.status);
+          return caches.match('offline/404.html');
+        }
+        return response;
+      });
+    }).catch(error => {
+      // If both fail, show a generic fallback:
+      console.log('Error: ', error);
+      return caches.match('offline/offline.html');
+    })
+  );
+});
